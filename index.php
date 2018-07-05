25e3ac1bf79f86d8e3e338366b9836e78fada73d
Bogdan Onischenko <dever4eg@gmail.com>
2018-07-05 12:11:16 +0300

    Add basic routing and templating

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+//Получаем адсес без параметров в переменную $route
+if(!empty($_SERVER['QUERY_STRING']))
+    $route = substr(
+        $_SERVER['REQUEST_URI'],0,
+        strpos($_SERVER['REQUEST_URI'], $_SERVER['QUERY_STRING'])-1);
+else
+    $route = $_SERVER['REQUEST_URI'];
+
+
+ob_start();
+
+if($route == "/" || $route == "/main") {
+
+    $products = require __DIR__ . '/products_data.php';
+    require __DIR__ . '/views/main.php';
+
+} elseif($route == "/details") {
+
+    $products = require __DIR__  . '/products_data.php';
+    $product = $products[$_GET['id']];
+
+
+    require __DIR__ . '/views/product.php';
+
+} elseif ($route == "/cart") {
+    require __DIR__ . '/views/cart.php';
+}
+
+$content = ob_get_clean();
+
+require __DIR__ . "/views/layout.php";
---
+++ products_data.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        "name"          => "Macbook",
+        "description"   => "<p>The MacBook is a brand of notebook computers manufactured by Apple Inc. from May 2006 to February 2012, and relaunched in 2015. Wikipedia</p>",
+    ],
+    [
+        "name"  => "Xiaomi notebook",
+        "description"  => "<p>The Xiaomi notebook is a brand of notebook computers manufactured by Xiaomi Inc. from May 2006 to February 2012, and relaunched in 2015. Wikipedia</p>",
+    ],
+    [
+        "name"  => "Xiaomi redmi note 4x",
+        "description"  => "<p>The Xiaomi phone is a brand of notebook computers manufactured by Xiaomi Inc. from May 2006 to February 2012, and relaunched in 2015. Wikipedia</p>",
+    ],
+    [
+        "name"  => "Xiaomi redmi note 4x",
+        "description"  => "<p>The Xiaomi phone is a brand of notebook computers manufactured by Xiaomi Inc. from May 2006 to February 2012, and relaunched in 2015. Wikipedia</p>",
+    ],
+    [
+        "name"  => "Xiaomi redmi note 4x",
+        "description"  => "<p>The Xiaomi phone is a brand of notebook computers manufactured by Xiaomi Inc. from May 2006 to February 2012, and relaunched in 2015. Wikipedia</p>",
+    ],
+];
