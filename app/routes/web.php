c8922c437e7e558105b0d6b511f1157f049f2088
Bogdan Onischenko <dever4eg@gmail.com>
2018-07-06 19:27:56 +0300

    Using new routing system

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,11 +3,36 @@
 use Src\Routing\Route;
 
 Route::add('get', '/', function () {
-    echo "home";
+    ob_start();
+
+    $products = require APP_PATH . '/products_data.php';
+    require APP_PATH . '/views/main.php';
+
+    $content = ob_get_clean();
+    require APP_PATH . "/views/layout.php";
+});
+
+Route::add('get', '/details', function () {
+    ob_start();
+
+
+    $products = require APP_PATH . '/products_data.php';
+    $product = $products[$_GET['id']];
+
+    require APP_PATH . '/views/product.php';
+
+    $content = ob_get_clean();
+    require APP_PATH . "/views/layout.php";
 });
 
 Route::add('get', '/cart', function () {
-    echo "cart";
+    ob_start();
+
+    require APP_PATH . '/views/cart.php';
+
+    $content = ob_get_clean();
+    require APP_PATH . "/views/layout.php";
 });
+
 
 Route::add('get', '/test', 'TestController@index');
