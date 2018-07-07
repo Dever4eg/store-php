956b7eeaa0aaa10954dc5cd8840cac43f8d6870f
Dever4eg <Dever4eg@gmail.com>
2018-07-07 15:56:43 +0300

    add method compare in route

--- src/Routing/RouteItem.php
+++ src/Routing/RouteItem.php
@@ -17,9 +17,26 @@ class RouteItem
 
     public function __construct($method, $url, $handler, $name = null)
     {
-        $this->method = $method;
+        $this->method = strtoupper($method);
         $this->url = $url;
         $this->handler = $handler;
         $this->name = $name;
+    }
+
+    public function match() : bool
+    {
+        //Удаляем из url параметры если есть
+        if(!empty($_SERVER['QUERY_STRING']))
+            $url = substr(
+                $_SERVER['REQUEST_URI'],0,
+                strpos($_SERVER['REQUEST_URI'], $_SERVER['QUERY_STRING'])-1);
+        else
+            $url = $_SERVER['REQUEST_URI'];
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if($this->url == $url && $this->method == $method)
+            return true;
+        return false;
     }
 }
