6e9de8a052e4a0487c8bf997e6aed544dd67300c
Dever4eg <Dever4eg@gmail.com>
2018-07-08 02:39:58 +0300

    add AppSingletonConponent interface

---
+++ src/App/AppSingletonComponent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dever
+ * Date: 7/8/18
+ * Time: 02:31
+ */
+
+namespace Src\App;
+
+
+interface AppSingletonComponent
+{
+    public function __construct();
+}
