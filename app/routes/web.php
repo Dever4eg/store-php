7e7a7240cd86a6fc09f159f8083f0cc0775ffbfb
Dever4eg <Dever4eg@gmail.com>
2018-07-10 02:23:53 +0300

    all view use twig

--- app/routes/web.php
+++ app/routes/web.php
@@ -6,36 +6,29 @@
  */
 
 use App\Controllers;
+use Src\View;
 
 $router = \Src\App::getRouter();
 
 $router->add('get', '/', function () {
     $products = require APP_PATH . '/products_data.php';
-    (new \Src\View("main") )
+    (new View("main") )
         ->withParam("products", $products)
         ->display();
 });
 
 $router->add('get', '/details', function () {
-    ob_start();
-
 
     $products = require APP_PATH . '/products_data.php';
     $product = $products[$_GET['id']];
 
-    require APP_PATH . '/views/product.php';
-
-    $content = ob_get_clean();
-    require APP_PATH . "/views/layout.twig";
+    (new View("product"))
+        ->withParam("product", $product)
+        ->display();
 });
 
 $router->add('get', '/cart', function () {
-    ob_start();
-
-    require APP_PATH . '/views/cart.php';
-
-    $content = ob_get_clean();
-    require APP_PATH . "/views/layout.twig";
+    (new View("cart"))->display();
 });
 
 
