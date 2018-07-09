3c3e547d586ed33942b2f9960d1dca519c57d647
Bogdan Onischenko <dever4eg@gmail.com>
2018-07-09 12:25:55 +0300

    revert index.php

--- public/index.php
+++ public/index.php
@@ -5,15 +5,4 @@ require_once __DIR__ . "/../vendor/autoload.php";
 const APP_PATH = __DIR__ . "/../app";
 const BASE_PATH = __DIR__ . "/..";
 
-//\Src\App::run();
-
-try {
-    $session = \Src\Session::instance();
-
-    $session->start();
-
-
-} catch (Exception $e) {
-    var_dump($e);
-}
-
+\Src\App::run();
