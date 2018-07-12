3a793d254beac1b7cf5a9d67fc14051c84fe0142
Bogdan Onischenko <dever4eg@gmail.com>
2018-07-12 13:59:26 +0300

    remove route for test controlles

--- App/routes/web.php
+++ App/routes/web.php
@@ -13,7 +13,6 @@ $router = \Src\App::getRouter();
 $router->add('get', '/', [new Controllers\ProductsController(), 'index']);
 $router->add('get', '/details', [new Controllers\ProductsController(), 'show']);
 $router->add('get', '/cart', [new View("cart"), 'display']);
-$router->add('get', '/test', [new Controllers\TestController, 'index']);
 
 $router->add('post', '/login', [new Controllers\AuthController(), 'login']);
 $router->add('get', '/logout', [new Controllers\AuthController(), 'logout']);
